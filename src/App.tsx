f43caf79697d73e5f0be14fa20f18215803ce890
Matteo Kinkela <matteo.kinkela@q-software.com>
2020-10-10 11:02:20 +0200

    fix(route): load routes into App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import {BrowserRouter} from "react-router-dom";
+import Routes from "./Routes";
 
 function App() {
   return (
-    <div className="App">
-      Pokedex
-    </div>
+    <BrowserRouter forceRefresh>
+      <Routes />
+    </BrowserRouter>
   );
 }
 
