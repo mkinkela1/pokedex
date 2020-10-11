d5771301c4ed95aeb0a0a4759be7fe64efeca92c
Matteo Kinkela <matteo.kinkela@q-software.com>
2020-10-11 13:19:50 +0200

    feat(logo button): click on logo button goes to default page

--- src/layouts/Main/components/Header/Header.tsx
+++ src/layouts/Main/components/Header/Header.tsx
@@ -3,6 +3,7 @@ import React, {useContext} from 'react';
 import LogoImage from './../../../../assets/svg/logo.svg';
 import MenuIcon from './../../../../assets/svg/menu.svg';
 import {MenuContext} from "../../../../contexts/MenuContext";
+import {Link} from "react-router-dom";
 
 const Header = () => {
 
@@ -11,7 +12,9 @@ const Header = () => {
   return (
     <section className="s-header">
       <div className="s-header__content">
-        <img src={LogoImage} alt="Pokemon logo" style={{height: 50}} />
+        <Link to={location => ({ ...location, pathname: `/pokedex` })}>
+          <img src={LogoImage} alt="Pokemon logo" style={{height: 50}} />
+        </Link>
         <button onClick={() => {
           if(toggleMenu)
             toggleMenu();
